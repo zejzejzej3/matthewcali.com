4638a677b0207c4c44962c127c4164a9f58b51d6
Matt Cali <dev@matthewcali.com>
2025-07-03 00:35:27 -0400

    12 update routing to use base url (#14)
    * feat: Update Astro Config and Main Layout
    
    - Added site and base to astro config
    - Imported helper component for using base url and updated href's
    
    * feat: Update services page link href's to use base_url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	server: { port: 9000 },
+	site: 'https://zejzejzej3.github.io',
+	base: '/matthewcali.com',
 	vite: {
 		plugins: [tailwindcss()]
 	}
